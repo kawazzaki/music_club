aaf82ffa85dfc8f0ddb2137dd5d75da5520a8a70
kawazzaki <nachreda02@gmail.com>
2025-02-16 11:03:21 -0800

    Updated work

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     window.scrollTo(0, 0);
 });
+window.addEventListener("load", function () {
+    document.body.style.visibility = "visible";
+    document.body.style.opacity = "1";
+});
